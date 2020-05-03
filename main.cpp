806308ba4f10511d8e8a649d093fd1d0eff5f228
Patrick Corcoran <gary.corcoran@mail.mcgill.ca>
2020-05-03 13:32:27 -0500

    Added erasing and colouring.

--- main.cpp
+++ main.cpp
@@ -1,25 +1,30 @@
 #include "sketch.h"
+
 void renderElephant () {
     int width = 600;
     int height = 600;
-    Point2f ear [] {{85.f, 505.f}, {240.f, 495.f}, {235.f, 295.f}, {80.f, 305.f}};
-    Point2f head [] {{170.f, 500.f}, {355.f, 500.f}, {355.f, 350.f}, {170.f, 350.f}};
-    Point2f eye [] {{305.f, 475.f}, {330.f, 475.f}, {330.f, 400.f}, {305.f, 400.f}};
-    Point2f iris [] {{305.f, 447.5f}, {315.f, 447.5f}, {315.f, 427.5f}, {305.f, 427.5f}};
-    Point2f mouth [] {{280.f, 404.f}, {280.f, 365.f}, {305.f, 365.f}};
-    Point2f cheek [] {{265.f, 420.f}, {265.f, 385.f}, {295.f, 385.f}, {295.f, 420.f}};
-    Point2f eyebrow [] {{285.f, 440.f}, {285.f, 515.f}, {330.f, 505.f}, {319.f, 469.f}};
-    Point2f trunkbot [] {{315.f, 370.f}, {360.f, 375.f}, {420.f, 340.f}, {435.f, 300.f}};
-    Point2f trunktop [] {{330.f, 415.f}, {380.f, 405.f}, {445.f, 360.f}, {470.f, 300.f}};
-    Point2f trunknose [] {{426.f, 323.f}, {471.f, 332.f}, {471.f, 300.f}, {426.f, 290.f}};
-    Point2f tline1 [] {{383.f, 365.f}, {388.f, 385.f}, {413.f, 395.f}};
-    Point2f tline2 [] {{395.f, 365.f}, {400.f, 380.f}, {421.f, 388.5f}};
-    Point2f body [] {{200.f, 350.f}, {300.f, 350.f}, {380.f, 190.f}, {250.f, 100.f}, {120.f, 190.f}};
-    Point2f arm [] {{196.f, 305.f}, {265.f, 305.f}, {300.f, 195.f}, {230.f, 170.f}, {160.f, 195.f}};
+    Point2f ear [] {{85.f, 505.f}, {240.f, 495.f}, {235.f, 295.f}, {80.f, 305.f},
+                    {85.f, 505.f}, {240.f, 495.f}};
+    Point2f head [] {{170.f, 495.f}, {355.f, 495.f}, {355.f, 345.f}, {170.f, 345.f}};
+    Point2f eye [] {{305.f, 470.f}, {330.f, 470.f}, {330.f, 395.f}, {305.f, 395.f}};
+    Point2f iris [] {{310.f, 444.5f}, {320.f, 444.5f}, {320.f, 424.5f}, {310.f, 424.5f}};
+    Point2f biris [] {{305.f, 455.f}, {325.f, 455.f}, {325.f, 410.f}, {305.f, 410.f}};
+    Point2f mouth [] {{280.f, 399.f}, {280.f, 360.f}, {305.f, 360.f}};
+    Point2f cheek [] {{265.f, 415.f}, {265.f, 380.f}, {295.f, 380.f}, {295.f, 415.f}};
+    Point2f eyebrow [] {{285.f, 435.f}, {285.f, 510.f}, {330.f, 500.f}, {319.f, 464.f}};
+    Point2f trunkbot [] {{315.f, 365.f}, {360.f, 370.f}, {420.f, 335.f}, {435.f, 295.f}};
+    Point2f trunktop [] {{330.f, 410.f}, {380.f, 400.f}, {445.f, 355.f}, {470.f, 295.f}};
+    Point2f trunknose [] {{426.f, 318.f}, {471.f, 327.f}, {471.f, 295.f}, {426.f, 285.f}};
+    Point2f tline1 [] {{383.f, 360.f}, {388.f, 380.f}, {413.f, 390.f}};
+    Point2f tline2 [] {{395.f, 360.f}, {400.f, 375.f}, {421.f, 383.5f}};
+    Point2f body [] {{200.f, 350.f}, {300.f, 350.f}, {380.f, 190.f}, {250.f, 100.f},
+                     {120.f, 190.f}};
+    Point2f arm [] {{196.f, 305.f}, {265.f, 305.f}, {300.f, 195.f}, {230.f, 170.f},
+                    {160.f, 195.f}};
     Point2f rleg [] {{300.f, 225.f}, {440.f, 210.f}, {430.f, 110.f}, {290.f, 130.f}};
     Point2f lleg [] {{320.f, 232.f}, {387.f, 257.f}, {455.f, 217.f}, {417.f, 130.f}};
-    Point2f hair1 [] {{265.f, 472.f}, {250.f, 530.f}, {270.f, 550.f}, {290.f, 550.f}};
-    Point2f hair2 [] {{270.f, 482.f}, {265.f, 520.f}, {280.f, 535.f}, {290.f, 535.f}};
+    Point2f hair1 [] {{265.f, 468.f}, {250.f, 525.f}, {270.f, 545.f}, {290.f, 545.f}};
+    Point2f hair2 [] {{270.f, 477.f}, {265.f, 515.f}, {280.f, 530.f}, {290.f, 530.f}};
     Point2f h1 [] {{188.f, 180.f}, {188.f, 197.f}, {214.f, 197.f}, {214.f, 162.f}};
     Point2f h2 [] {{214.f, 160.f}, {214.f, 202.f}, {246.f, 202.f}, {246.f, 160.f}};
     Point2f h3 [] {{246.f, 160.f}, {246.f, 197.f}, {274.f, 197.f}, {274.f, 177.f}};
@@ -30,15 +35,14 @@ void renderElephant () {
     Point2f f1 [] {{418.f, 120.f}, {380.f, 120.f}, {390.f, 200.f}, {434.f, 212.f}};
     Point2f f2 [] {{415.f, 205.f}, {410.f, 210.f}, {420.f, 240.f}, {430.f, 230.f}};
     Point2f rl1 [] {{403.f, 185.5f}, {380.f, 190.f}, {385.f, 215.f}, {410.f, 210.f}};
-    Point2f rl2 [] {{400.f, 162.f}, {369.f, 167.f}, {377.f, 191.f}, {403.f, 183.f}};
+    Point2f rl2 [] {{400.f, 162.f}, {369.f, 167.f}, {377.f, 191.f}, {403.f, 185.f}};
     Point2f ll1 [] {{425.f, 223}, {400.f, 228.f}, {405.f, 245.f}, {430.f, 235.f}};
-    Point2f ll2 [] {{425.f, 205}, {395.f, 210.f}, {400.f, 230.f}, {430.f, 220.f}};
-
-
-    Part parts [] {{ear, sizeof(ear) / sizeof(*ear), true},
+    Point2f ll2 [] {{428.f, 205}, {395.f, 210.f}, {400.f, 230.f}, {430.f, 220.f}};
+    Part parts [] {{ear, sizeof(ear) / sizeof(*ear), false},
                    {head, sizeof(head) / sizeof(*head), true},
                    {eye, sizeof(eye) / sizeof(*eye), true},
                    {iris, sizeof(iris) / sizeof(*iris), true},
+                   {biris, sizeof(biris) / sizeof(*biris), true},
                    {mouth, sizeof(mouth) / sizeof(*mouth), false},
                    {cheek, sizeof(cheek) / sizeof(*cheek), false},
                    {eyebrow, sizeof(eyebrow) / sizeof(*eyebrow), false},
@@ -66,11 +70,44 @@ void renderElephant () {
                    {rl2, sizeof(rl2) / sizeof(*rl2), false},
                    {ll1, sizeof(ll1) / sizeof(*ll1), false},
                    {ll2, sizeof(ll2) / sizeof(*ll2), false}};
-
     Sketch sketch {width, height};
-    for (int i = 0; i < sizeof(parts) / sizeof(*parts); i++) {
+    sketch.setBackgroundColour({1.f, 1.f, 1.f});
+    sketch.setDrawColour({0.f, 0.f, 0.f});
+    for (int i = 0; i < sizeof(parts) / sizeof(*parts); ++i) {
         sketch.drawCurve(parts[i]);
+        // sketch.colourPart(parts[i], {1.f, 128.f/255.f, 149.f/255.f});
     }
+    sketch.erase({155, 157}, {173, 177});
+    sketch.erase({431, 309}, {457, 327});
+    sketch.erase({334, 369}, {356, 405});
+    sketch.erase({221, 445}, {235, 480});
+    sketch.erase({232, 345}, {274, 351});
+    sketch.erase({215, 293}, {245, 309});
+    sketch.erase({288, 168}, {324, 204});
+    sketch.erase({351, 220}, {361, 233});
+    sketch.erase({168, 348}, {206, 485});
+    sketch.erase({177, 302}, {184, 321});
+    sketch.erase({184, 309}, {201, 333});
+    sketch.erase({200, 329}, {222, 360});
+    sketch.erase({222, 334}, {225, 352});
+    sketch.erase({225, 345}, {229, 351});
+    sketch.erase({229, 347}, {231, 349});
+    sketch.erase({355, 219}, {357, 221});
+    sketch.erase({310, 140}, {357, 204});
+    sketch.erase({351, 204}, {357, 218});
+    sketch.erase({288, 481}, {324, 495});
+    sketch.colourObject({width / 2, height / 2}, {1.f, 128.f/255.f, 149.f/255.f});
+    sketch.colourObject({427, 210}, {1.f, 128.f/255.f, 149.f/255.f});
+    sketch.colourObject({413, 166}, {1.f, 128.f/255.f, 149.f/255.f});
+    sketch.colourObject({110, 170}, {0.f, 0.f, 0.f});
+    sketch.colourObject({230, 185}, {0.9f, 0.75f, 0.f});
+    sketch.colourObject({204, 190}, {0.9f, 0.75f, 0.f});
+    sketch.colourObject({260, 186}, {0.9f, 0.75f, 0.f});
+    sketch.colourObject({384, 177}, {0.9f, 0.75f, 0.f});
+    sketch.colourObject({392, 202}, {0.9f, 0.75f, 0.f});
+    sketch.colourObject({408, 217}, {0.9f, 0.75f, 0.f});
+    sketch.colourObject({413, 234}, {0.9f, 0.75f, 0.f});
+    sketch.colourObject({317, 420}, {0.f, 0.f, 0.f});
     sketch.write();
 }
 
--- sketch.h
+++ sketch.h
@@ -7,7 +7,7 @@
 struct Part {
     Point2f* pts;
     int npts;
-    bool closed;
+    bool closed = false;
 };
 
 class Sketch {
@@ -31,6 +31,13 @@ private:
     }
 public:
     Sketch(int width, int height) : m_width(width), m_height(height) { }
+    void erase(Point botleft, Point topright) {
+        for (int j = botleft.y; j < topright.y; ++j) {
+            for (int i = botleft.x; i < topright.x; ++i) {
+                *img[j * m_width + i] = m_bg;
+            }
+        }
+    }
     void setBackgroundColour(Vector colour) {
         m_bg = colour;
         for (int j = 0; j < m_height; j++) {
@@ -75,5 +82,38 @@ public:
         else {
             _drawCurve(part.pts, part.npts);
         }
+    }
+    void grow(int i, int j, Vector colour) {
+        Vector p = *img[j * m_width + i];
+        if (p.x == m_fg.x && p.y == m_fg.y && p.z == m_fg.z) {
+            return;
+        }
+        if (p.x == colour.x && p.y == colour.y && p.z == colour.z) {
+            return;
+        }
+        *img[j * m_width + i] = colour;
+        grow(i + 1, j, colour);
+        grow(i - 1, j, colour);
+        grow(i, j + 1, colour);
+        grow(i, j - 1, colour);
+    }
+    void colourPart(Part part, Vector colour) {
+        Point2f inside {0.f, 0.f};
+        for (int i = 0; i < part.npts; ++i) {
+            inside.x += part.pts[i].x;
+            inside.y += part.pts[i].y;
+        }
+        inside.x /= part.npts;
+        inside.y /= part.npts;
+        inside.x = round(inside.x);
+        inside.y = round(inside.y);
+        std::cout << inside.x << ", " << inside.y << std::endl;
+        grow(inside.x, inside.y, colour);
+        std::cout << "done" << std::endl;
+    }
+    void colourObject(Point p, Vector colour) {
+        int i = m_width / 2;
+        int j = m_height / 2;
+        grow(p.x, p.y, colour);
     }
 };
